5fb25ba26bb477345e5cae875329bf55a0015ec6
mac <mac@nat-oitwireless-inside-vapornet100-10-8-17-23.princeton.edu>
2019-09-11 22:08:17 -0400

    model connected to database

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from Insta.models import Post
+
 # Register your models here.
+# show the content defined in this app that should
+# appear in admin's model
+admin.site.register(Post)
+# migration to make the change take effect
